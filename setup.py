7c0160904a4babc2577d86753039d6b044d3796b
Darwin Monroy <contact@darwinmonroy.com>
2015-10-23 22:48:53 -0600

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='chilero',
-    version='0.1.6',
+    version='0.1.7',
     namespace_packages=['chilero'],
     packages=find_packages(),
     include_package_data=True,
